cda87633e3d7e64fe25a7f69f7cd69060fe22422
Stuart Boydell <stuart@codemaster.com.au>
2019-01-31 02:57:40 +1100

    add extension framework

---
+++ out/extension.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "unibasic" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World!');
+    });
+    context.subscriptions.push(disposable);
+    // context.subscriptions.push(vscode.languages.registerHoverProvider('unibasic',
+    //  new 
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+// class GoHoverProvider implements HoverProvider {
+//    public provideHover(
+//        document: TextDocument, position: Position, token: CancellationToken):
+//        Thenable<Hover> {
+//           return new vscode.Hover("something hovering");
+//        }
+//    }
+// }
+// export function activate(ctx: vscode.ExtensionContext): void {
+//    ctx.subscriptions.push(
+//        vscode.languages.registerHoverProvider(
+//            GO_MODE, new GoHoverProvider()));
+//        })
+// }
+//# sourceMappingURL=extension.js.map
